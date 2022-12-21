caf1e626540459807339e3622941974ef5c5a514
dimon777ddd <gxgamer727@gmail.com>
2022-12-21 21:43:01 +0400

    first task

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine("The first number is equal to the second number");
+}
+
+else if (number1 > number2)
+{
+    Console.WriteLine("First number is bigger then second number");
+}
+
+else
+{
+    Console.WriteLine("Second number is bigger then first number");
+}
